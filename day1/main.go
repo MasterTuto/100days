bfa4181bfe3c3d2fc514f30e7d1037088d226161
MasterTuto <brenocarvalho2011@gmail.com>
2022-06-02 15:40:10 -0300

    day1

---
+++ day1/main.go
@@ -0,0 +1,19 @@
+// Day 1: Hello, world!
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	// using fmt
+	fmt.Println("Hello, world!")
+
+	// using log
+	log.Println("Hello, world!")
+
+	// using builtin function
+	println("Hello, world!")
+}
